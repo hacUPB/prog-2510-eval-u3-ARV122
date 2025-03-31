fc54f73fb50f06ae376d9481ea0bc10ce7c78273
ARV122 <andres.ruizv@upb.edu.co>
2025-03-31 17:47:14 -0500

    Esta es la bitacora

---
+++ Bitacora/Ejercicio3.py
@@ -0,0 +1,19 @@
+print("Programacion 2025")
+print('Ingenieria Aeronautica')
+
+print("UPB", "Medellin", "Antioquia")
+print("UPB" "Medellin" "Antioquia")
+
+
+#Ejercicio 3
+
+print("Programacion")
+print()
+print("bloque 9")
+
+print("Programacion", end=" * ")
+print("bloque 9")
+
+# correcion de sintaxis 
+print("yo te lo dije: \"hola como estas?\" ") 
+print('yo te lo dije: "Hola como estas?" ')
---
+++ Bitacora/Ejercicio4.py
@@ -0,0 +1,12 @@
+#Utilizamos la funcion print 
+
+nombre = 'Andres'
+edad = 23
+estatura = 1.75
+
+print("Soy", nombre, "y tengo", edad, "años")
+print(f"Soy {nombre} y mido {estatura}")
+
+print('&' * 20)
+print('*' * 15)
+print('%' * 10)
